96674d4ce6ed6f1d4085d07b2e68bf92136ea5c2
Rusiru Devinda <116636125+RUSIRUDEVINDA@users.noreply.github.com>
2025-01-28 11:47:57 +0530

    Build loggers

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from helper import preprocessing, vectorizer, get_prediction
+from logger import logging
 
 app = Flask(__name__)
+
+logging.info("Flask server started")#take configurations from logger
 
 data = dict()
 reviews = []
@@ -14,14 +17,24 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info("=======Open home page=======")
+
     return render_template("index.html", data=data)
 
 @app.route("/", methods=['post'])
 def my_post():
     text = request.form['text']  # Handle missing 'text' key gracefully
+    logging.info(f'Text : {text}')
+
     preprocessed_txt = preprocessing(text)
+    logging.info(f'Preprocessed_txt  : { preprocessed_txt }')
+
     vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'Vectorized_txt  : {vectorized_txt}')
+
     prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction : {prediction}') 
 
     global positive, negative  # Declare as global to modify
     if prediction == 'negative':
